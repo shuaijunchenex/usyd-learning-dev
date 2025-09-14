9f9135d8db1f3d3508b2188a488df903d3c17401
shuaijunchenex <46318558+shuaijunchenex@users.noreply.github.com>
2025-09-15 07:22:56 +1000

    Update model_utils.py

--- src/usyd_learning/ml_utils/model_utils.py
+++ src/usyd_learning/ml_utils/model_utils.py
@@ -7,6 +7,18 @@ from torch import nn
 class ModelUtils:
 
     @staticmethod
+    def accelerator_device():
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+        else:
+            device = torch.device("cpu")
+
+        console.info(f"Using device: {device}")
+        return device
+
+    @staticmethod
     def clear_all(model: nn.Module, optimizer: Optimizer):
         """
         Clears gradients, resets optimizer state, and releases unused cached GPU memory.
