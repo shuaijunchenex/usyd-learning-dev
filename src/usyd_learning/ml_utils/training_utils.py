4a5644e3b46578efdead0c3d36672b9bbb23765a
shuaijunchenex <46318558+shuaijunchenex@users.noreply.github.com>
2025-09-19 01:35:41 +1000

    Update training_utils.py

--- src/usyd_learning/ml_utils/training_utils.py
+++ src/usyd_learning/ml_utils/training_utils.py
@@ -3,31 +3,30 @@ import numpy as np
 import torch
 
 class TrainingUtils:
-    
+
     @staticmethod
-    def set_seed(seed: int = 42):
+    def set_seed(seed_input: int = 42):
         # Python 内置随机库
-        random.seed(seed)
+        random.seed(seed_input)
         # NumPy
-        np.random.seed(seed)
+        np.random.seed(seed_input)
         # PyTorch (CPU)
-        torch.manual_seed(seed)
+        torch.manual_seed(seed_input)
         # PyTorch (GPU, 单卡)
         if torch.cuda.is_available():
-            torch.cuda.manual_seed(seed)
-            torch.cuda.manual_seed_all(seed)  # 多卡
+            torch.cuda.manual_seed(seed_input)
+            torch.cuda.manual_seed_all(seed_input)  # 多卡
 
         # 保证 cudnn 使用确定性算法（会牺牲一些性能）
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
 
-        np.random.seed(seed)
+        np.random.seed(seed_input)
         # PyTorch (CPU)
-        torch.manual_seed(seed)
+        torch.manual_seed(seed_input)
         # PyTorch (GPU, 单卡)
         if torch.cuda.is_available():
-            torch.cuda.manual_seed(seed)
-            torch.cuda.manual_seed_all(torch.seed)
-
+            torch.cuda.manual_seed(seed_input)
+            
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
